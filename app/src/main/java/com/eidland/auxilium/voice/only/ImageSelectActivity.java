686ebbee0afd8fe5d424db35674ee76a9c104c8d
zahinraidah <56842235+zahinraidah@users.noreply.github.com>
2021-06-18 01:40:37 +0600

    changed Avatar Selection View

--- app/src/main/java/com/eidland/auxilium/voice/only/ImageSelectActivity.java
+++ app/src/main/java/com/eidland/auxilium/voice/only/ImageSelectActivity.java
@@ -3,9 +3,11 @@ package com.eidland.auxilium.voice.only;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridView;
 
 import androidx.annotation.Nullable;
 
+import com.eidland.auxilium.voice.only.adapter.ImageAdapter;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
@@ -14,6 +16,8 @@ public class ImageSelectActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_select);
+        GridView gridview = findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
     }
 
     public void ChangeImage(View view) {
@@ -22,5 +26,6 @@ public class ImageSelectActivity extends Activity {
                 .setAspectRatio(1, 1)
                 .setRequestedSize(1000, 1000, CropImageView.RequestSizeOptions.RESIZE_EXACT)
                 .start(ImageSelectActivity.this);
+
     }
 }
