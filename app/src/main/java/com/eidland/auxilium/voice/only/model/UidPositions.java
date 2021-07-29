58d8c0b5bec241feaa672c15e6e563222ca73dd7
auxhallnetwork <mity.crystal@gmail.com>
2021-07-29 23:29:31 +0600

    support files

---
+++ app/src/main/java/com/eidland/auxilium/voice/only/model/UidPositions.java
@@ -0,0 +1,37 @@
+package com.eidland.auxilium.voice.only.model;
+
+import android.content.Context;
+import android.view.View;
+
+import com.eidland.auxilium.voice.only.helper.PortraitAnimator;
+
+public class UidPositions {
+   public int uid;
+   public int position;
+   public boolean isfill=false;
+    private PortraitAnimator mAnimator;
+    public UidPositions(int uid, int position){
+        this.uid=uid;
+        this.position=position;
+    }
+  public   void initAnimator(Context context, View layer1, View layer2) {
+        mAnimator = new PortraitAnimator(context, layer1, layer2);
+    }
+
+    public void setIsfill(boolean isfill) {
+        this.isfill = isfill;
+    }
+
+
+    public   void startAnimation() {
+        if (mAnimator != null) {
+            mAnimator.startAnimation();
+        }
+    }
+
+  public   void stopAnimation() {
+        if (mAnimator != null) {
+            mAnimator.forceStop();
+        }
+    }
+}
