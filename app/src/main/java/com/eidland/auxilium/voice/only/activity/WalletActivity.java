1eb59488a5fd719482518606092f1e8ac4726f8f
auxhallnetwork <mity.crystal@gmail.com>
2021-11-11 01:17:35 +0600

    fixed coin reflection issue

--- app/src/main/java/com/eidland/auxilium/voice/only/activity/WalletActivity.java
+++ app/src/main/java/com/eidland/auxilium/voice/only/activity/WalletActivity.java
@@ -1,5 +1,6 @@
 package com.eidland.auxilium.voice.only.activity;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
@@ -13,9 +14,11 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingResult;
+import com.android.billingclient.api.Purchase;
 import com.anjlab.android.iab.v3.BillingProcessor;
 import com.anjlab.android.iab.v3.PurchaseInfo;
-import com.anjlab.android.iab.v3.TransactionDetails;
 import com.eidland.auxilium.voice.only.R;
 import com.eidland.auxilium.voice.only.helper.Helper;
 import com.eidland.auxilium.voice.only.model.StaticConfig;
@@ -24,19 +27,22 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public abstract class WalletActivity extends AppCompatActivity implements View.OnClickListener {
+public class WalletActivity extends AppCompatActivity implements View.OnClickListener {
     TextView txtcurrent;
     LinearLayout buy50, buy1000, buy350;
-    //  BillingProcessor billingProcessor;
+    BillingProcessor billingProcessor;
     String coincomma, Userid;
     ViewDialog viewDialog;
     ImageView back;
-    int width = 0;
-
+    int width=0;
+    boolean productpurchasedcalled=false;
+    boolean verifycalled=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,38 +71,132 @@ public abstract class WalletActivity extends AppCompatActivity implements View.O
         buy1000.setOnClickListener(this);
         buy350.setOnClickListener(this);
 
-        //  initpur();
-    }
-}
-/*
-    void purchasecoins() {
-        billingProcessor.purchase(this, selectedkey);
+        initpur();
     }
 
+    void purchasecoins() {
+billingProcessor.purchase(this, selectedkey);
+
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (selectedkey==null)
+        {
+
+        }
+        else {
+            if(productpurchasedcalled)
+            {
+
+            }
+            else {
+
+                manualonProductPurchased(selectedkey,billingProcessor.getPurchaseInfo(selectedkey));
+                selectedkey=null;
+            }
+        }
+
+
+    }
+    public void initpur() {
         boolean isAvailable = BillingProcessor.isIabServiceAvailable(this);
         if (isAvailable) {
-            billingProcessor = new BillingProcessor(this, getResources().getString(R.string.Playid), this);
-            billingProcessor.initialize();
+
+
+            billingProcessor = new BillingProcessor(this, getResources().getString(R.string.Playid),  new BillingProcessor.IBillingHandler() {
+                @Override
+                public void onProductPurchased(@NonNull String productId, @Nullable PurchaseInfo purchaseInfo) {
+                    productpurchasedcalled=true;
+                    if (!verifycalled)
+                    {
+                        Log.d("enteredwallaet", "onProductPurchased: Hello "+productId);
+                        Userid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                        String currentCoins = StaticConfig.user.getCoins();
+                        long coininnumber = Long.parseLong(currentCoins);
+                        long finalcoin = coininnumber + Long.parseLong(productId);
+                        StaticConfig.user.setCoins(finalcoin + "");
+                        billingProcessor.consumePurchaseAsync(selectedkey, new BillingProcessor.IPurchasesResponseListener()
+                        {
+                            @Override
+                            public void onPurchasesSuccess()
+                            {
+                                //  Toast.makeText(getApplicationContext(),"Successfully consumed",Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            @Override
+                            public void onPurchasesError()
+                            {
+                                // Toast.makeText(getApplicationContext(),"Not consumed",Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+                        selectedkey=null;
+                        verifypurchase();
+                    }
+
+                }
+                @Override
+                public void onBillingError(int errorCode, @Nullable Throwable error) {
+                    //Toast.makeText(getApplicationContext(),"onBillingError: " + Integer.toString(errorCode),Toast.LENGTH_SHORT).show();
+                }
+                @Override
+                public void onBillingInitialized() {
+                   // Toast.makeText(getApplicationContext(),"onBillingInitiated: " ,Toast.LENGTH_SHORT).show();
+
+
+                }
+                @Override
+                public void onPurchaseHistoryRestored() {
+
+                }
+
+            });
         }
     }
+    public void manualonProductPurchased(@NonNull String productId, @Nullable PurchaseInfo purchaseInfo) {
+        productpurchasedcalled=true;
+        Log.d("enteredwallaet", "onProductPurchased: Hello "+productId);
+        Userid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        String currentCoins = StaticConfig.user.getCoins();
+        long coininnumber = Long.parseLong(currentCoins);
+        long finalcoin = coininnumber + Long.parseLong(productId);
+        StaticConfig.user.setCoins(finalcoin + "");
+        billingProcessor.consumePurchaseAsync(selectedkey, new BillingProcessor.IPurchasesResponseListener()
+        {
+            @Override
+            public void onPurchasesSuccess()
+            {
+                //  Toast.makeText(getApplicationContext(),"Successfully consumed",Toast.LENGTH_SHORT).show();
 
+            }
+
+            @Override
+            public void onPurchasesError()
+            {
+                // Toast.makeText(getApplicationContext(),"Not consumed",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        selectedkey=null;
+        verifypurchase();
+    }
 
     @Override
     public void onDestroy() {
         if (billingProcessor != null) {
-         //   billingProcessor.release();
+            billingProcessor.release();
         }
         super.onDestroy();
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-      //  if (!billingProcessor.handleActivityResult(requestCode, resultCode, data))
-            super.onActivityResult(requestCode, resultCode, data);
-    }
 
-    @Override
-   /* public void onProductPurchased(String productId, TransactionDetails details) {
+
+  /*  @Override
+    public void onProductPurchased(String productId, PurchaseInfo purchaseInfo) {
         Log.d("enteredwallaet", "onProductPurchased: Hello "+productId);
         Userid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         String currentCoins = StaticConfig.user.getCoins();
@@ -105,23 +205,8 @@ public abstract class WalletActivity extends AppCompatActivity implements View.O
         StaticConfig.user.setCoins(finalcoin + "");
 
         verifypurchase();
-    }/
+    }*/
 
-
-    @Override
-    public void onPurchaseHistoryRestored() {
-
-    }
-
-    @Override
-    public void onBillingError(int errorCode, Throwable error) {
-
-    }
-
-    @Override
-    public void onBillingInitialized() {
-
-    }
 
     ProgressDialog progressDialog;
 
@@ -130,70 +215,76 @@ public abstract class WalletActivity extends AppCompatActivity implements View.O
         FirebaseDatabase.getInstance().getReference("Users").child(Userid).setValue(StaticConfig.user).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                Toast.makeText(WalletActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+              //  Toast.makeText(WalletActivity.this, "Saved", Toast.LENGTH_SHORT).show();
                 coincomma = Helper.getFormattedText(StaticConfig.user.getCoins());
                 txtcurrent.setText(coincomma);
-                Toast.makeText(WalletActivity.this, "Purchased Successfully", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(WalletActivity.this, "Purchased Successfully", Toast.LENGTH_SHORT).show();
                 // lrnmethod.setVisibility(View.GONE);
                 viewDialog.hideDialog();
 
 
             }
         });
+        verifycalled=true;
     }
 
 
-    String selectedkey = "";
+    String selectedkey = null;
 
     @Override
     public void onClick(View v) {
+        productpurchasedcalled=false;
+        verifycalled=false;
         switch (v.getId()) {
 
             case R.id.buy50:
                 selectedkey = "50";
-
+                purchasecoins();
                 break;
             case R.id.buy350:
                 selectedkey = "350";
-
+               purchasecoins();
                 break;
             case R.id.buy1000n:
                 selectedkey = "1000";
-
+               purchasecoins();
                 break;
 
 
         }
-        calldialog();
+       //calldialog();
 
     }
-void calldialog ()
-{
-    Dialog dialog = new Dialog(this);
-    dialog.setContentView(R.layout.layout_custom_dialog);
-    LinearLayout linearLayout = dialog.findViewById(R.id.alert_root);
-    linearLayout.setMinimumWidth((int) (width * 0.8));
-    //dialog.getWindow().setBackgroundDrawableResource(R.drawable.white_corner);
-    dialog.setCancelable(false);
 
-    ImageView imageView = dialog.findViewById(R.id.dialog_icon);
-    imageView.setImageResource(R.drawable.ic_sleep);
-    imageView.setVisibility(View.GONE);
 
-    TextView msg = dialog.findViewById(R.id.msg);
-    msg.setText("Coin purchases are coming very soon");
+    void calldialog ()
+    {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.layout_custom_dialog);
+        LinearLayout linearLayout = dialog.findViewById(R.id.alert_root);
+        linearLayout.setMinimumWidth((int) (width * 0.8));
+        //dialog.getWindow().setBackgroundDrawableResource(R.drawable.white_corner);
+        dialog.setCancelable(false);
 
-    TextView negative = dialog.findViewById(R.id.positive_btn);
-    RelativeLayout areatop = dialog.findViewById(R.id.topdialogbutton);
-    negative.setVisibility(View.VISIBLE);
-    negative.setText("OKAY");
-    areatop.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            dialog.dismiss();
-        }
-    });
-    dialog.show();
+        ImageView imageView = dialog.findViewById(R.id.dialog_icon);
+        imageView.setImageResource(R.drawable.ic_sleep);
+        imageView.setVisibility(View.GONE);
+
+        TextView msg = dialog.findViewById(R.id.msg);
+        msg.setText("Coin purchases are coming very soon");
+
+        TextView negative = dialog.findViewById(R.id.positive_btn);
+        RelativeLayout areatop = dialog.findViewById(R.id.topdialogbutton);
+        negative.setVisibility(View.VISIBLE);
+        negative.setText("OKAY");
+        areatop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+
 }
-}
-*/
